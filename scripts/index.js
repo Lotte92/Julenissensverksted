9f4ad93c36a2c9a4f878c9d2fe31e00aa33b4900
Charlotte Nagelsen <cnagelsen@gmail.com>
2021-12-07 10:45:00 +0100

    La til javascript fil som endrer bakgrunnsfarge på siden

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    document.body.style.backgroundColor="#DDA0DD";
+    for (element of document.getElementsByClassName("container")) {
+        element.style.backgroundColor="#DDA0DD";
+    }
+}
